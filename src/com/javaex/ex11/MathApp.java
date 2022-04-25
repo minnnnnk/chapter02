77df2a3819d0615b8a288e80a4c6a0ff2269c536
mingyu <jubal9153@gmail.com>
2022-04-25 14:57:26 +0900

    chapter02

---
+++ src/com/javaex/ex11/MathApp.java
@@ -0,0 +1,16 @@
+package com.javaex.ex11;
+
+public class MathApp {
+
+	public static void main(String[] args) {
+		System.out.println(Math.plus(3, 1));
+		System.out.println(Math.plus(3.2, 1.0));
+		
+		
+		System.out.println(Math.circleArea(5));
+		
+		
+		
+	}
+
+}
